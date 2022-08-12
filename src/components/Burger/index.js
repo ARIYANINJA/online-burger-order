fda2e4519e5fc71f5f808c1bee1ad429c1911fe7
ARIYANINJA <ariyaninj@gmail.com>
2022-08-12 18:23:29 +0800

    doneBurger

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import BurgerIngredient from '../BurgerIngredient'
+import styles from './style.module.css'
 function Burger() {
     return ( 
-        <div>
-            <BurgerIngredient type = 'bread-top'></BurgerIngredient>
+        <div className={styles.Burger}>
+            <BurgerIngredient type = 'bread-top'>
+    <BurgerIngredient class="seed"></BurgerIngredient>
+    <BurgerIngredient class="seed second"></BurgerIngredient>
+    <BurgerIngredient class="seed third"></BurgerIngredient>
+    <BurgerIngredient class="seed fourth"></BurgerIngredient>    
+            </BurgerIngredient>
             <BurgerIngredient type = 'salad'></BurgerIngredient>
             <BurgerIngredient type = 'cheese'></BurgerIngredient>
             <BurgerIngredient type = 'meat'></BurgerIngredient>
--- src/components/BurgerIngredient/index.js
+++ src/components/BurgerIngredient/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import styles from './style.module.css'
 function BurgerIngredient(props) {
-    return ( 
-        <div>
-            <p>{props.type}</p>
-            <p>{props.type}</p>
-            <p>{props.type}</p>
-            <p>{props.type}</p>
-        </div>
-     );
+
+if(props.type === 'salad') return <div className={styles.Salad}></div>
+if(props.type === 'meat') return <div className={styles.Meat}></div>
+if(props.type === 'bacon') return <div className={styles.Bacon}></div>
+if(props.type === 'bread-top') return (<div className={styles.BreadTop}>
+    <div className={styles.Seed}></div>
+    <div className={`${styles.Seed} ${styles.Second}`}></div>
+    <div className={`${styles.Seed} ${styles.Third}`}></div>
+    <div className={`${styles.Seed} ${styles.Fourth}`}></div>
+</div>)
+if(props.type === 'seed') return <div className={styles.Seed}></div>
+if(props.type === 'cheese') return <div className={styles.Cheese}></div>
+if(props.type === 'bread-bottom') return <div className={styles.BreadBottom}></div>
+if(props.type === 'first') return <div className={styles.First}></div>
+
+
+
 }
 
 export default BurgerIngredient;
