16b536204892792b0b39b38303074dda0c9aeeec
ARIYANINJA <ariyaninj@gmail.com>
2022-08-12 16:23:58 +0800

    initProject

---
+++ src/components/Toolbar/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function Toolbar() {
+    return ( 
+        <div>
+            <h1>
+                i am toolbar component
+            </h1>
+        </div>
+     );
+}
+
+export default Toolbar;
---
+++ src/pages/BurgerBuilder/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+function BurgerBuilder() {
+    return ( 
+        <div>
+            <p>khachirtai talh</p>
+            <p>control</p>
+        </div>
+     );
+}
+
+export default BurgerBuilder;
